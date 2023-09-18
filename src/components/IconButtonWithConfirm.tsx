a62c0f291f1232f507011236b15984d300cd7af5
riel <riel@caresharing.ph>
2023-09-18 10:50:17 +0800

    Pass icon button circulal progress icon props

--- src/components/IconButtonWithConfirm.tsx
+++ src/components/IconButtonWithConfirm.tsx
@@ -1,27 +1,34 @@
 import {
   CircularProgress,
+  type CircularProgressProps,
   IconButton,
   type IconButtonProps,
 } from '@mui/material'
 import AlertDialog, { type AlertDialogProps } from './AlertDialog'
 import { useToggle } from '..'
 
+const DEFAULT_CIRCULAR_PROGRESS_PROPS = {
+  size: 16,
+}
+
 type Props = Omit<IconButtonProps, 'onClick'> & {
   loading?: boolean
   alertDialogProps: Omit<AlertDialogProps, 'open' | 'onClose'>
+  circularProgressProps: CircularProgressProps
 }
 
 export default function ButtonWithConfirm({
   loading,
   children,
   alertDialogProps,
+  circularProgressProps = DEFAULT_CIRCULAR_PROGRESS_PROPS,
   ...props
 }: Props) {
   const toggle = useToggle()
   return (
     <>
       <IconButton {...props} onClick={toggle.toggle}>
-        {loading ? <CircularProgress size={16} /> : children}
+        {loading ? <CircularProgress {...circularProgressProps} /> : children}
       </IconButton>
       <AlertDialog
         {...alertDialogProps}
