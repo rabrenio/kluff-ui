5d8a20e0b3ec333f2379d374a2f0e1461d4335f4
Riel Stephen Abrenio <rabrenio@users.noreply.github.com>
2023-09-28 10:24:41 +0800

    Riel dev (#4)
    * Fix build definition files not included in build
    
    ---------
    
    Co-authored-by: riel <riel@caresharing.ph>

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,6 @@
 export * from './components';
+export { default as generateMuiTheme } from './utils/theme';
+export { default as useToggle } from './utils/useToggle';
+export { default as useMenuState } from './utils/useMenuState';
+export * from './utils/objectFields';
+export * from './utils/date';
