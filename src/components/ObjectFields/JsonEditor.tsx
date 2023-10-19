5da01684de5c723bd6888ac2371891fcf25259b6
riel <riel@caresharing.ph>
2023-10-19 11:01:41 +0800

    Fix json editor mode not working when readOnly or disabled values are updated

--- src/components/ObjectFields/JsonEditor.tsx
+++ src/components/ObjectFields/JsonEditor.tsx
@@ -1,10 +1,11 @@
-import { type ComponentProps, type ReactNode, forwardRef } from 'react'
+import { type ComponentProps, type ReactNode, forwardRef, useMemo } from 'react'
 import {
   FormControl,
   type FormControlProps,
   FormHelperText,
   FormLabel,
 } from '@mui/material'
+import uniqueId from 'lodash.uniqueid'
 import { JsonEditor as Editor } from 'jsoneditor-react'
 import ace from 'brace'
 import 'brace/mode/json'
@@ -35,20 +36,27 @@ const JsonEditor = forwardRef<HTMLDivElement, JsonEditorProps>(
       ...props
     },
     ref
-  ) => (
-    <FormControl {...containerProps} error={error}>
-      {label && <FormLabel required={required}>{label}</FormLabel>}
-      <Editor
-        {...props}
-        value={value}
-        innerRef={ref}
-        ace={ace}
-        mode={disabled || readOnly ? 'view' : 'code'}
-        theme="ace/theme/github"
-      />
-      {helperText && <FormHelperText>{helperText}</FormHelperText>}
-    </FormControl>
-  )
+  ) => {
+    // mode is bugged when value is change
+    // to fix, force rerender when disabled or readOnly are changed
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const key = useMemo(() => uniqueId('JsonEditor'), [disabled, readOnly])
+    return (
+      <FormControl {...containerProps} error={error}>
+        {label && <FormLabel required={required}>{label}</FormLabel>}
+        <Editor
+          {...props}
+          key={key}
+          value={value}
+          innerRef={ref}
+          ace={ace}
+          mode={disabled || readOnly ? 'view' : 'code'}
+          theme="ace/theme/github"
+        />
+        {helperText && <FormHelperText>{helperText}</FormHelperText>}
+      </FormControl>
+    )
+  }
 )
 
 export default JsonEditor
